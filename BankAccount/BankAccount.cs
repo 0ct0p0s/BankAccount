6c4e25f06802b84f4dbf40d9bb27baf750e49ab8
0ct0p0s <seanmacgillis@gmail.com>
2020-06-30 09:58:19 -0400

    new file

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/*Key Features
+ * 10 digit number that is unique
+ * balance can be stored with the name or names of the owners
+ * The balance can be retrieved
+ * It accepts deposits
+ * It accepts withdrawals
+ * The initial balance must be positive
+ * You cannot overdraw the account
+ */
+
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        public string accountNumber { get; set; }
+        public string accountOwner { get; set; }
+        public decimal accountBalance { get; set; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.accountOwner = name;
+            this.accountBalance = initialBalance;
+        }
+
+        public static void makeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public static void makeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
